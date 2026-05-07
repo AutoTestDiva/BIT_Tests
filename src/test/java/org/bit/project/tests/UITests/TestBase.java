61f73fa1f22385a831e4664faf1d653d68cfa93e
AutoTestDiva <nataliia.autoqa@gmail.com>
2026-05-07 18:28:14 +0200

    5 commit

--- src/test/java/org/bit/project/tests/UITests/TestBase.java
+++ src/test/java/org/bit/project/tests/UITests/TestBase.java
@@ -1,8 +1,9 @@
 package org.bit.project.tests.UITests;
 
-//import org.bit.project.utils.MyListener;
+import org.bit.project.utils.MyListener;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.Browser;
@@ -11,42 +12,96 @@ import org.openqa.selenium.support.events.WebDriverListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 
 import java.lang.reflect.Method;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestBase {
-
-//    Logger logger = LoggerFactory.getLogger(TestBase.class);
-//    String browser = System.getProperty("browser", Browser.CHROME.browserName());
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+    String browser = System.getProperty("browser", Browser.CHROME.browserName());
     public WebDriver driver;
 
 //    @BeforeMethod
-//    public void startLogger() {
+//    public void init() {
 //        System.err.close();
 //        if (browser.equalsIgnoreCase(Browser.CHROME.browserName())) {
-//            driver = new ChromeDriver();
-//            logger.info("All test run in Chrome Browser");
+//            ChromeOptions options = new ChromeOptions();
+//
+//            // убрать сообщение "Chrome is being controlled..."
+//            options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
+//
+//            // отключение сервисов Chrome
+//            options.addArguments("--disable-notifications");
+//            options.addArguments("--disable-popup-blocking");
+//            options.addArguments("--disable-default-apps");
+//            options.addArguments("--no-default-browser-check");
+//
+//            // отдельный профиль Selenium
+//            options.addArguments("user-data-dir=C:/selenium/chrome-profile");
+//
+//            // настройки Chrome
+//            Map<String, Object> prefs = new HashMap<>();
+//
+//            // отключить уведомления
+//            prefs.put("profile.default_content_setting_values.notifications", 2);
+//
+//            // отключить менеджер паролей
+//            prefs.put("credentials_enable_service", false);
+//            prefs.put("profile.password_manager_enabled", false);
+//
+//            options.setExperimentalOption("prefs", prefs);
+//            driver = new ChromeDriver(options);
+//            logger.info("All tests run in Chrome Browser");
 //        } else if (browser.equalsIgnoreCase(Browser.FIREFOX.browserName())) {
 //            driver = new FirefoxDriver();
-//            logger.info("All test run in Firefox Browser");
+//            logger.info("All tests run in Firefox Browser");
 //        } else if (browser.equalsIgnoreCase(Browser.EDGE.browserName())) {
 //            driver = new EdgeDriver();
-//            logger.info("All test run in Edge Browser");
+//            logger.info("All tests run in Edge Browser");
 //        }
-//
+//        // Listener
 //        WebDriverListener listener = new MyListener();
 //        driver = new EventFiringDecorator(listener).decorate(driver);
 //
-//        driver.get("http://localhost:8080/cms.html");
-//        logger.info("The link --> " + driver.getCurrentUrl());
-//        logger.info("**********************************************************");
 //        driver.manage().window().maximize();
 //        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.get("http://localhost:8080/cms.html");
+//
+//        logger.info("Open URL -> " + driver.getCurrentUrl());
+//        logger.info("************************************************");
 //    }
+    @BeforeMethod
+    public void startLogger(Method m) {
+        logger.info("****************************************************************");
+        logger.info("Start method --> " + m.getName());
+    }
+    @BeforeMethod
+    public void startLogger() {
+        System.err.close();
+        if (browser.equalsIgnoreCase(Browser.CHROME.browserName())) {
+            driver = new ChromeDriver();
+            logger.info("All test run in Chrome Browser");
+        } else if (browser.equalsIgnoreCase(Browser.FIREFOX.browserName())) {
+            driver = new FirefoxDriver();
+            logger.info("All test run in Firefox Browser");
+        }else if (browser.equalsIgnoreCase(Browser.EDGE.browserName())) {
+            driver = new EdgeDriver();
+            logger.info("All tests run in Edge Browser");
+        }
+
+        WebDriverListener listener = new MyListener();
+        driver = new EventFiringDecorator(listener).decorate(driver);
+
+        driver.get("http://localhost:8080/cms.html");
+        logger.info("The link --> " + driver.getCurrentUrl());
+        logger.info("**********************************************************");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
 
     @BeforeMethod
     public void init() {
@@ -55,11 +110,15 @@ public class TestBase {
         driver.get("http://localhost:8080/cms.html");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.quit();
+
+        if (driver != null) {
+            logger.info("Closing browser");
+            driver.quit();
+            logger.info("Browser closed");
+        }
     }
 }
